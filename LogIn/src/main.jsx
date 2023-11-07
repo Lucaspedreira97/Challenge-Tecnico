d436884e04caae9c6e583179ec1d1845daec22dd
Lucas Pedreira <pedreiralucasm@gmail.com>
2023-11-07 09:59:37 -0300

    Auth0 config

--- LogIn/src/main.jsx
+++ LogIn/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-yd266ykn0elfjscw.us.auth0.com"
+      clientId="Yx3vM2bG3tng4I6HsDY4Pm0Q1NwBh3Eu"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    ,
+  </React.StrictMode>
+);
--- LogIn/src/routes/index.routes.jsx
+++ LogIn/src/routes/index.routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/LogIn";
+import Login from "../pages";
 
 
 
